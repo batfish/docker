2d4813fadc236504e7b65abcb67132ec97ce3b2d
Spencer Fraint <spencer@intentionet.com>
2018-09-21 14:33:40 -0700

    use xenial, simplify install

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,17 +1,8 @@
 set -x -e
 
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-   ### install python packages
-   echo -e "\n   ............. Installing pip"
-   sudo -H apt-get -y install python-pip || exit 1
-   pip --version || exit 1
-elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-   echo $PATH
-   export PATH=/usr/local/share/python:$PATH
-   java -version || exit 1
-   javac -version || exit 1
-   which pip || easy_install pip || exit 1
-   pip --version || exit 1
+if [[ ${TRAVIS_OS_NAME} == 'linux' ]]; then
+   sudo apt-get update
+   sudo apt-get install python3.5 python3-pip
 else
    echo "Unsupported TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
    exit 1 # CI not supported in this case
