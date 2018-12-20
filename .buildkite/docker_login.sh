178c4bc0ebbff8b1541501aa362247dd0e51a117
Ari Fogel <ari@fogelti.me>
2018-12-19 22:02:36 -0800

    fix login script

--- .buildkite/docker_login.sh
+++ .buildkite/docker_login.sh
@@ -1,9 +1,9 @@
 #/usr/bin/env bash
-docker login --username="${DOCKER_BOT_USER}" --password-stdin <(python3 <<EOF
+{ python3 <<EOF
 import os
 print(os.environ['DOCKER_BOT_PASSWORD'])                                                                              
-EOF
-)
+EOF 
+} | docker login --username="${DOCKER_BOT_USER}" --password-stdin
 unset DOCKER_BOT_PASSWORD
 unset DOCKER_BOT_USER
 
