763f39b19ba1e8ca1fbd36c2b9eb3ae1971a8a73
Ari Fogel <ari@fogelti.me>
2018-12-19 22:05:26 -0800

    fix whitespace

--- .buildkite/docker_login.sh
+++ .buildkite/docker_login.sh
@@ -2,7 +2,7 @@
 { python3 <<EOF
 import os
 print(os.environ['DOCKER_BOT_PASSWORD'])                                                                              
-EOF 
+EOF
 } | docker login --username="${DOCKER_BOT_USER}" --password-stdin
 unset DOCKER_BOT_PASSWORD
 unset DOCKER_BOT_USER
