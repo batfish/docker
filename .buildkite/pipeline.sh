120d5d5f88740c9379d3970bc13e36e5546f6c21
Ari Fogel <ari@fogelti.me>
2018-12-19 19:35:56 -0800

    use docker login hook

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,16 +5,11 @@ cat <<EOF
 steps:
   - label: "Build Images"
     command: 
-      - "python3 .buildkite/write_password.py"
-      - "unset DOCKER_BOT_PASSWORD"
-      - "docker login --username=batfishbuildkitebot --password-stdin < docker_bot_password"
       - ".buildkite/build.sh"
     plugins:
       - docker#v2.1.0:
           image: "arifogel/batfish-docker-build-base:latest"
           always-pull: true
-          environment:
-            - "DOCKER_BOT_PASSWORD"
           volumes:
             - ".:/workdir"
             - "/var/run/docker.sock:/var/run/docker.sock"
