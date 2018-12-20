6d089e2517fb5881977f4ab18978a0f782060080
Ari Fogel <ari@fogelti.me>
2018-12-19 19:08:26 -0800

    better bootstrap

--- .buildkite/bootstrap.sh
+++ .buildkite/bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-CONFIG_FILE="/usr/local/etc/buildkite-agent/buildkite-agent.cfg"
+CONFIG_FILE="/etc/buildkite-agent/buildkite-agent.cfg"
 
 put_config() {
   KEY="$1"
   VALUE="$2"
-  if grep "${KEY}" "${CONFIG_FILE}"; then
+  if grep "${KEY}" "${CONFIG_FILE}" >&/dev/null; then
     sed -i -e "s/${KEY}=.*/${KEY}=${VALUE}/g" "${CONFIG_FILE}"
   else
+    mkdir -p "$(dirname "${CONFIG_FILE}")"
     echo "${KEY}=${VALUE}" >> "${CONFIG_FILE}"
   fi
 }
