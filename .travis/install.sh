5e6a0b366019377895b071c68f698f21bfb26fe0
Spencer Fraint <spencer@intentionet.com>
2019-05-09 10:26:04 -0700

    Remove travis files (#37)

--- .travis/install.sh
+++
@@ -1,9 +0,0 @@
-set -x -e
-
-if [[ ${TRAVIS_OS_NAME} == 'linux' ]]; then
-   sudo apt-get update
-   sudo apt-get install python3.5 python3-pip
-else
-   echo "Unsupported TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
-   exit 1 # CI not supported in this case
-fi
