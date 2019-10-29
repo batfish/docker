857d32ee4540675ccfe4938ef47958e70f9c0606
Spencer Fraint <spencer@intentionet.com>
2019-10-28 17:00:01 -0700

    add tests to unified pipeline

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -23,6 +23,9 @@ DOCKER_PLUGIN_VERSION="${DOCKER_PLUGIN_VERSION:-v3.3.0}"
 
 BATFISH_VERSION_STRING="${BATFISH_VERSION_STRING:-$(date +'%Y.%m.%d').${BUILDKITE_BUILD_NUMBER}}"
 
+# Only test Bf containers less than this many days old
+BATFISH_MAX_TEST_CONTAINER_AGE="${BATFISH_MAX_TEST_CONTAINER_AGE:-31}"
+
 # Attributes common to all command steps
 COMMON_STEP_ATTRIBUTES=
 # Specify queue for upload builds
@@ -145,6 +148,54 @@ cat <<EOF
       - docker-login#${DOCKER_LOGIN_PLUGIN_VERSION}:
           username: ${DOCKER_LOGIN_PLUGIN_USERNAME}
           password-env: DOCKER_LOGIN_PLUGIN_PASSWORD
+${COMMON_STEP_ATTRIBUTES}
+EOF
+
+###### WAIT between container upload and final testing
+cat <<EOF
+  - wait
+EOF
+
+# Get (Unix time) timestamp for the oldest container we would test
+MIN_TIMESTAMP=$(date -d "$(date +%Y-%m-%d) - ${BATFISH_MAX_TEST_CONTAINER_AGE} day" +%s)
+
+CONTAINER_TAGS=$(wget -q -O - https://registry.hub.docker.com/v1/repositories/batfish/batfish/tags)
+# Get tags that start with dates (YYYY.M.D.#)
+DATE_TAGS=$(echo "$CONTAINER_TAGS" | grep -o '"[0-9]\{4\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}\(\.[0-9]\+\)\?"' | sed 's/"//g')
+
+# Run integration tests on recent Batfish containers
+while read bf_tag; do
+# Convert YYYY.M.D format into (Unix time) timestamp that we can compare
+TAG_TIMESTAMP=$(date -d $(echo ${bf_tag} | grep -o '[0-9]\{4\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}' | sed 's/\./-/g') +"%s")
+if [[ ${MIN_TIMESTAMP} -le ${TAG_TIMESTAMP} ]]; then
+cat <<EOF
+  - label: ":snake: dev <-> :batfish: ${bf_tag}"
+    if: pipeline.id == "${BATFISH_UPLOAD_PIPELINE}"
+    command:
+      - ".buildkite/test_batfish_container.sh"
+    env:
+      BATFISH_CONTAINER_TAG: ${bf_tag}
+      # Skip notebook ref tests
+      PYBATFISH_PYTEST_ARGS: '-k "not test_notebook_output"'
+${COMMON_STEP_ATTRIBUTES}
+EOF
+fi
+done <<< "${DATE_TAGS}"
+
+cat <<EOF
+  - label: ":snake: dev <-> :batfish: prod"
+    if: pipeline.id == "${BATFISH_UPLOAD_PIPELINE}"
+    command:
+      - ".buildkite/test_batfish_container.sh"
+    env:
+      BATFISH_CONTAINER_TAG: latest
+      # Skip notebook ref tests
+      PYBATFISH_PYTEST_ARGS: '-k "not test_notebook_output"'
+${COMMON_STEP_ATTRIBUTES}
+  - label: ":snake: dev <-> :batfish: dev"
+    if: pipeline.id == "${BATFISH_UPLOAD_PIPELINE}"
+    command:
+      - ".buildkite/test_batfish_container.sh"
 ${COMMON_STEP_ATTRIBUTES}
 EOF
 
